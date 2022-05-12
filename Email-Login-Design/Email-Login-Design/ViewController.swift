04f89b5c3bd6f6fff40992939b428d6d59cd487d
Krushnapanchal8 <86007243+Krushnapanchal8@users.noreply.github.com>
2022-05-12 13:22:53 +0530

    Add files via upload

---
+++ Email-Login-Design/Email-Login-Design/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Email-Login-Design
+//
+//  Created by Mac on 10/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
